f73efbb40234986a6d1b5ac6807259a5ea5e6bf8
4117 Borisova E. I <lizkaborisova@list.ru>
2024-03-11 19:36:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_project_6sem/MainActivity2.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_project_6sem
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main2)
+
+    }
+}
